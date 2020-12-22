90c3fefaa9500b842f7670f32ef349d866aecc66
Otis Chiemezie <otisidevelop@gmail.com>
2020-12-22 20:13:30 +0100

    Update Langs.js

--- intl/Langs.js
+++ intl/Langs.js
@@ -1,5 +1,13 @@
 import en from "./lang/en.json";
+import fr from "./lang/fr.json";
+import de from "./lang/de.json";
+import pl from "./lang/pl.json";
+import es from "./lang/es.json";
 
 export const LangStrings = {
-    en
+    en,
+    fr,
+    de,
+    pl,
+    es
 };
