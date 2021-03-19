9f62b971d9aa07a885b3121dcefc2c35ea5b8185
Otis Chiemezie <77255740+reviewdownloader@users.noreply.github.com>
2021-03-19 09:45:18 +0100

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -29,14 +29,14 @@ export const Footer = () => {
                                         </li>
 
                                         <li>
-                                            <a href="https://app.timostephaninvestmentservice.com/create-account">{t("nav.signup")}</a>
+                                            <a href="https://app.timostephaninvestment.com/create-account">{t("nav.signup")}</a>
                                         </li>
                                     </ul>
                                 </div>
                                 <div className="col-md-6 col-sm-6">
                                     <ul className="footer-nav">
                                         <li>
-                                            <a href="https://app.timostephaninvestmentservice.com">{t("nav.login")}</a>
+                                            <a href="https://app.timostephaninvestment.com">{t("nav.login")}</a>
                                         </li>
                                         <li>
                                             <a href="/faqs">{t("nav.faqs")}</a>
