9d8b46f44c147fb0907d4ec922a59cb2206c2406
Otis Chiemezie <info.reviewdownloader@gmail.com>
2021-03-23 23:15:03 +0100

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -35,7 +35,7 @@ export default function About() {
                                     </h3>
                                     <div className="section-heading-line line-thin"></div>
                                     <div className="text-content">
-                                        <p>{t("pp.about-s")}</p>
+                                        <p>{t("app.about-s")}</p>
                                     </div>
                                 </div>
                                 <div className="primary-list mt-25">
